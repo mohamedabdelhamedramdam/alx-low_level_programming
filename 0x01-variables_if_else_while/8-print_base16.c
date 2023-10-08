9fcbb8ec548c5676b2d3ed7985596ba06b80ad62
mohamedabdelhamedramdam <mohamadabdelhamed14@gmail.com>
2023-10-08 07:09:14 -0700

    7 , 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,6 +3,7 @@
  * main - Entry point of the program
  *
  * Description: This program prints the digits from '0' to '9'
+ * followed by the lowercase letters from 'a' to 'f', all on the same line,
  * followed by a newline character.
  *
  * Return: 0 (Success)
@@ -11,8 +12,8 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a <= 9; a++)
-		printf("%d", a);
+	for (a = 0; a <= 15; a++)
+		putchar(a);
 	putchar('\n');
 	return (0);
 }
