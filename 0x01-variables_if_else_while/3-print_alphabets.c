6052a5f5e1f5139583714e63d4f75a74d60e27fa
mohamedabdelhamedramdam <mohamadabdelhamed14@gmail.com>
2023-10-08 04:55:28 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,16 +3,19 @@
  * main - Entry point of the program
  *
  * Description: This program prints the lowercase alphabet from 'a' to 'z'
+ * followed by the uppercase alphabet from 'A' to 'Z', all on the same line,
  * followed by a newline character.
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
-	char a;
+	char a, A;
 
 	for (a = 'a'; a <= 'z'; a++)
 		putchar(a);
+	for (A = 'A'; A <= 'Z'; A++)
+		putchar(A);
 	putchar('\n');
 	return (0);
 }
