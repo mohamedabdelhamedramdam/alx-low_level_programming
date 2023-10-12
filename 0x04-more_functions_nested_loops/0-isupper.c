e1ecad1fab9a4cf58e6df9e3d0c61895b3e64e96
mohamedabdelhamedramdam <mohamadabdelhamed14@gmail.com>
2023-10-12 13:32:21 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+/**
+ * _isupper - Checks for uppercase character
+ * @c: The character to be checked
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
