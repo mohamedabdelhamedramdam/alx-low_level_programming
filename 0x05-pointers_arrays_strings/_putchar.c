5d2c5778e9ce08a50de3ad0817c26358700fefea
mohamedabdelhamedramdam <mohamadabdelhamed14@gmail.com>
2023-10-16 14:52:09 -0700

    all

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,13 +1,13 @@
-nclude <unistd.h>
+#include <unistd.h>
 
 /**
- *  * _putchar - Writes a character to stdout
- *   * @c: The character to be written
- *    *
- *     * Return: On success 1
- *      * On error, -1 is returned and errno is set appropriately
- *       */
+ * _putchar - Writes a character to stdout
+ * @c: The character to be written
+ *
+ * Return: On success 1
+ * On error, -1 is returned and errno is set appropriately
+ */
 int _putchar(char c)
 {
-		return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
