4277717b2ae9db7e7a35de68305785370d5b4508
mohamedabdelhamedramdam <mohamadabdelhamed14@gmail.com>
2023-10-27 12:16:17 -0700

    all

---
+++ 0x09-static_libraries/c-files/100-atoi.c
@@ -0,0 +1,29 @@
+int _atoi(char *s)
+{
+	int num, a, flag;
+
+	flag = 1;
+	a = num = 0;
+
+	while (((s[a] < '0') || s[a] > '9') && s[a] != 0)
+	{
+		if (s[a] == '-')
+			flag = flag * -1;
+		a++;
+	}
+	while (((s[a] >= '0') && s[a] <= '9') && s[a] != 0)
+	{
+		if (num >= 0)
+		{
+			num = num * 10 - (s[a] - '0');
+			a++;
+		}
+		else
+		{
+			num = num * 10 - (s[a] - '0');
+			a++;
+		}
+	}
+	flag = flag * -1;
+	return (num * flag);
+}
